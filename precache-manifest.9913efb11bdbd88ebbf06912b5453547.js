c728855bccc656d1eedf08847fede11e442ef14d
joker_ <zhangzhe_sdu@icloud.com>
2020-07-20 21:08:26 +0800

    deploy

---
+++ precache-manifest.9913efb11bdbd88ebbf06912b5453547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239ab78a219d72eef0f1",
+    "url": "/xiao-qi-Bookkeeping-website/css/app.30932a57.css"
+  },
+  {
+    "revision": "3a6cbd645d995e582fabc43d55224b9b",
+    "url": "/xiao-qi-Bookkeeping-website/index.html"
+  },
+  {
+    "revision": "239ab78a219d72eef0f1",
+    "url": "/xiao-qi-Bookkeeping-website/js/app.0a57cb94.js"
+  },
+  {
+    "revision": "f7cba58f3141f707044b",
+    "url": "/xiao-qi-Bookkeeping-website/js/chunk-vendors.d880cda6.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/xiao-qi-Bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xiao-qi-Bookkeeping-website/robots.txt"
+  }
+]);
